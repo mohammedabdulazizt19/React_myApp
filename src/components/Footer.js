524c7a07793566a7f97948f9e4913d8b7f62f0af
Abdulaziz <mohammed.abdulazizt.19@gmail.com>
2025-07-16 14:11:46 +0800

    Adding footer css stylesheet

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,8 @@
 import React from "react";
-
+import "./footer.css";
 const Footer = () => {
   return (
-    <footer>
+    <footer className="myfooter">
       <p>This is footer</p>
     </footer>
   );
